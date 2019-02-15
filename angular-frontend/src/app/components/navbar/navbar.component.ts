7d9e7ab4c398b00cee208758ad79129c133234ef
Finn Sell <dev@finn-thorben.me>
2019-02-16 00:55:56 +0100

    fix: Update navbar to include login information

--- angular-frontend/src/app/components/navbar/navbar.component.ts
+++ angular-frontend/src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {UserService} from '../../../network/providers/user/user.service';
+import {UserService} from '../../../network/rest-clients/user/user.service';
+import {AuthService} from '../../../auth/auth.service';
 
 @Component({
     selector: 'app-navbar',
@@ -8,7 +9,7 @@ import {UserService} from '../../../network/providers/user/user.service';
 })
 export class NavbarComponent implements OnInit {
 
-    constructor(protected userService: UserService) {
+    constructor(protected userService: UserService, protected authService: AuthService) {
     }
 
     ngOnInit() {
