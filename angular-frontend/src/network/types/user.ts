10e59fa430b07cf221b87bcfbe33afaba912b22b
Finn Sell <dev@finn-thorben.me>
2019-02-17 15:27:54 +0100

    feat: Update User data type to reflect new API

--- angular-frontend/src/network/types/user.ts
+++ angular-frontend/src/network/types/user.ts
@@ -1,10 +1,9 @@
-import {Team} from './team';
 import {ImageSet} from './imageSet';
 
 export interface User<T extends 'simple' | 'resolved'> {
     id: number;
     username: string;
-    teams: T extends 'simple' ? number[] : Team<'simple'>[];
+    teams: T extends 'simple' ? number[] : {id: number, name: string}[];
     points: number;
     pinnedSets: T extends 'simple' ? number[] : ImageSet[];
 }
