58d5d7ebbbf0645152a7b0f3437d1e11b3b33a16
Finn Sell <dev@finn-thorben.me>
2019-02-18 16:08:17 +0100

    fix: Fix bug where imageset resolve failure would not be handled at all

--- angular-frontend/src/app-images/imageset/imageset-resolver.service.ts
+++ angular-frontend/src/app-images/imageset/imageset-resolver.service.ts
@@ -3,7 +3,7 @@ import {ImageSetService} from '../../network/rest-clients/image-set.service';
 import {ActivatedRouteSnapshot, ParamMap, Resolve, Router, RouterStateSnapshot} from '@angular/router';
 import {ImageSet} from '../../network/types/imageSet';
 import {EMPTY, Observable, of, zip} from 'rxjs';
-import {flatMap, map, tap} from 'rxjs/operators';
+import {catchError, flatMap, map, tap} from 'rxjs/operators';
 import {UserService} from '../../network/rest-clients/user.service';
 
 
@@ -21,28 +21,20 @@ export class ImagesetResolverService implements Resolve<ImagesetData> {
     }
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<ImagesetData> {
-        const imageSet$ = this.resolveImageset(route.paramMap);
-
-        // Merge all resolved resources together
-        return zip(
-            imageSet$
-        ).pipe(
-            flatMap(result => of({
-                set: result[0]
-            } as ImagesetData))
-        );
-    }
-
-    private resolveImageset(paramMap: ParamMap): Observable<ImageSet> {
-        const id = paramMap.get('imagesetId');
+        const id = route.paramMap.get('imagesetId');
         if (+id) {
             return this.imageSetService.read(+id).pipe(
-                tap(result => {
-                    if (!result) {
-                        this.router.navigate(['/404']);
-                    }
-                })
+                map(value => {
+                    return {
+                        set: value
+                    } as ImagesetData;
+                }),
+                catchError(flatMap(() => {
+                    this.router.navigate(['/404']);
+                    return EMPTY;
+                }))
             );
+
         } else {
             this.router.navigate(['/404']);
             return EMPTY;
