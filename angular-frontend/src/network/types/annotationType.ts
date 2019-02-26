119b7e5b58687bc3d2396eaec1fe3d3f84b2d281
Finn Sell <dev@finn-thorben.me>
2019-02-26 11:29:29 +0100

    fix: AnnotationType.VectorType now works correctly

--- angular-frontend/src/network/types/annotationType.ts
+++ angular-frontend/src/network/types/annotationType.ts
@@ -1,18 +1,19 @@
 export interface AnnotationType {
-  id: number;
-  name: string;
-  active: boolean;
-  vectorType: VectorType;   // TODO Check if this actually works
-  nodeCount: number;
-  enableConcealed: boolean;
-  enableBlurred: boolean;
+    id: number;
+    name: string;
+    active: boolean;
+    vectorType: VectorType;   // TODO Check if this actually works
+    nodeCount: number;
+    enableConcealed: boolean;
+    enableBlurred: boolean;
 }
 
 
-enum VectorType {
-  boundingBox,
-  point,
-  line,
-  multiLine,
-  polygon
+export enum VectorType {
+    INVALID,
+    boundingBox,
+    point,
+    line,
+    multiLine,
+    polygon
 }
