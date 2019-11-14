4a8bf8bd6ccdf20b2a191ffd63cbd1e5436dc269
c.marzahl@de.eu.local <c.marzahl@euroimmun.de>
2019-11-14 11:02:47 +0100

    Import PNG, JPG oder Container images

---
+++ util/slide_server.py
@@ -0,0 +1,58 @@
+from collections import OrderedDict
+from io import BytesIO
+import openslide
+from openslide import OpenSlide, OpenSlideError, open_slide
+from openslide.deepzoom import DeepZoomGenerator
+import os
+from optparse import OptionParser
+from threading import Lock
+
+SLIDE_CACHE_SIZE = 10
+DEEPZOOM_FORMAT = 'jpeg'
+DEEPZOOM_TILE_SIZE = 254
+DEEPZOOM_OVERLAP = 1
+DEEPZOOM_LIMIT_BOUNDS = True
+DEEPZOOM_TILE_QUALITY = 75
+
+
+
+class PILBytesIO(BytesIO):
+    def fileno(self):
+        '''Classic PIL doesn't understand io.UnsupportedOperation.'''
+        raise AttributeError('Not supported')
+
+
+class SlideCache(object):
+    def __init__(self, cache_size):
+        self.cache_size = cache_size
+        self._lock = Lock()
+        self._cache = OrderedDict()
+
+    def get(self, path):
+        with self._lock:
+            if path in self._cache:
+                # Move to end of LRU
+                slide = self._cache.pop(path)
+                self._cache[path] = slide
+                return slide
+
+        osr = open_slide(path)
+        slide = DeepZoomGenerator(osr)
+        try:
+            mpp_x = osr.properties[openslide.PROPERTY_NAME_MPP_X]
+            mpp_y = osr.properties[openslide.PROPERTY_NAME_MPP_Y]
+            slide.mpp = (float(mpp_x) + float(mpp_y)) / 2
+        except (KeyError, ValueError):
+            slide.mpp = 0
+
+        with self._lock:
+            if path not in self._cache:
+                if len(self._cache) == self.cache_size:
+                    self._cache.popitem(last=False)
+                self._cache[path] = slide
+        return slide
+
+class SlideFile(object):
+    def __init__(self, relpath):
+        self.name = os.path.basename(relpath)
+        self.url_path = relpath
