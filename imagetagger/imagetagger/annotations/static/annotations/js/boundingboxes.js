325d86574bb250d75ffff1a5149f5833fa816118
c.marzahl@de.eu.local <c.marzahl@euroimmun.de>
2019-11-19 09:32:56 +0100

    Some clean up

--- imagetagger/imagetagger/annotations/static/annotations/js/boundingboxes.js
+++ imagetagger/imagetagger/annotations/static/annotations/js/boundingboxes.js
@@ -4,7 +4,6 @@ class BoundingBoxes {
     constructor(annotationTypeId, noSelection, viewer) {
         this.initialized = false;
         this.selection = undefined;
-        this.selected_id = undefined;
         this.vector_type = 1;
         if (globals.image === '') {
             globals.image = $('#image');
@@ -25,9 +24,6 @@ class BoundingBoxes {
 
         let border_size = 2;
         var boundingBox = document.createElement('div');
-        boundingBox.addEventListener('click', function (event) {
-            this.selected_id = $(event.toElement).data().annotationid;
-        });
         boundingBox.setAttribute('class', 'boundingBox');
         boundingBox.setAttribute('id', 'boundingBox' + annotation.id);
         $(boundingBox).data('annotationid', annotation.id);
@@ -72,65 +68,13 @@ class BoundingBoxes {
 
     removeAnnotation(annotationid) {
         this.hEl.removeElementById('boundingBox' + annotationid);
-        //this.updateView();
     }
 
     updateView() {
-        const currentZoom = this.viewer.viewport.getZoom(true) + 0.01;
+        const currentZoom = this.viewer.viewport.getZoom(true) + 0.0001;
         const oldCenter = this.viewer.viewport.getCenter(true);
         this.viewer.viewport.zoomTo(currentZoom, oldCenter, true);
     }
-
-    setHighlightColor(id) {
-        let highlightBox;
-        for (let box of $('.boundingBox')) {
-            if ($(box).data('annotationid') === id) {
-                highlightBox = box;
-                break;
-            }
-        }
-        if (highlightBox) {
-            $(highlightBox).css({
-                //'border': '3px solid ' + globals.mutColor
-            });
-        }
-    }
-
-    unsetHighlightColor(id) {
-        let highlightBox;
-        for (let box of $('.boundingBox')) {
-            if ($(box).data('annotationid') === id) {
-                highlightBox = box;
-                break;
-            }
-        }
-        if (highlightBox) {
-            $(highlightBox).css({
-                //'border': '2px solid ' + globals.stdColor
-            });
-        }
-    }
-
-
-    /**
-     * Initialize the selection.
-     */
-    initSelection() {
-        return;
-
-        this.initialized = true;
-
-        this.selection = globals.image.imgAreaSelect({
-            instance: true,
-            show: true,
-            minHeight: 2,
-            minWidth: 2,
-            onSelectChange: this.updateAnnotationFields,
-            resizeMargin: 3
-        });
-        this.selection.cancelSelection();
-    }
-
 
     /**
      * Reload the selection.
@@ -169,164 +113,6 @@ class BoundingBoxes {
     }
 
     /**
-     * Restore the selection.
-     */
-    restoreSelection(reset) {
-        if (!$('#keep_selection').prop('checked')) {
-            return;
-        }
-        if (globals.restoreSelection !== undefined) {
-            if (globals.restoreSelection === null) {
-                $('#not_in_image').prop('checked', true);
-                $('#coordinate_table').hide();
-            } else {
-                $('#x1Field').val(globals.restoreSelection.x1);
-                $('#x2Field').val(globals.restoreSelection.x2);
-                $('#y1Field').val(globals.restoreSelection.y1);
-                $('#y2Field').val(globals.restoreSelection.y2);
-                this.reloadSelection(0, globals.restoreSelection);
-            }
-        }
-        if (reset !== false) {
-            globals.restoreSelection = undefined;
-        }
-    }
-
-    moveSelectionUp() {
-        // calculate value +/- stepsize (times stepsize to account for differing image sizes)
-        let newValueY1 = Math.round(parseInt($('#y1Field').val()) - Math.max(1, (globals.moveSelectionStepSize * globals.imageScaleHeight)));
-        let newValueY2 = Math.round(parseInt($('#y2Field').val()) - Math.max(1, (globals.moveSelectionStepSize * globals.imageScaleHeight)));
-        // checking if the box would be out of bounds and puts it to max/min size and doesn't move the other dimension
-        if (newValueY1 < 0) {
-            newValueY1 = 0;
-            newValueY2 = $('#y2Field').val();
-        }
-        if (newValueY2 > Math.round(globals.image.height() * globals.imageScaleHeight)) {
-            newValueY2 = Math.ceil(globals.image.height() * globals.imageScaleHeight);
-            newValueY1 = $('#y1Field').val();
-        }
-        // update values
-        $('#y1Field').val(newValueY1);
-        $('#y2Field').val(newValueY2);
-        this.reloadSelection();
-    }
-
-    moveSelectionDown() {
-        // calculate value +/- stepsize times stepsize to account for differing image sizes
-        let newValueY1 = Math.round(parseInt($('#y1Field').val()) + Math.max(1, (globals.moveSelectionStepSize * globals.imageScaleHeight)));
-        let newValueY2 = Math.round(parseInt($('#y2Field').val()) + Math.max(1, (globals.moveSelectionStepSize * globals.imageScaleHeight)));
-        // checking if the box would be out of bounds and puts it to max/min size and doesn't move the other dimension
-        if (newValueY1 < 0) {
-            newValueY1 = 0;
-            newValueY2 = $('#y2Field').val();
-        }
-        if (newValueY2 > Math.round(globals.image.height() * globals.imageScaleHeight)) {
-            newValueY2 = Math.ceil(globals.image.height() * globals.imageScaleHeight);
-            newValueY1 = $('#y1Field').val();
-        }
-        // update values
-        $('#y1Field').val(newValueY1);
-        $('#y2Field').val(newValueY2);
-        this.reloadSelection();
-    }
-
-    moveSelectionRight() {
-        // calculate value +/- stepsize times stepsize to account for differing image sizes
-        let newValueX1 = Math.round(parseInt($('#x1Field').val()) + Math.max(1, (globals.moveSelectionStepSize * globals.imageScaleWidth)));
-        let newValueX2 = Math.round(parseInt($('#x2Field').val()) + Math.max(1, (globals.moveSelectionStepSize * globals.imageScaleWidth)));
-        // checking if the box would be out of bounds and puts it to max/min size and doesn't move the other dimension
-        if (newValueX1 < 0) {
-            newValueX1 = 0;
-            newValueX2 = $('#x2Field').val();
-        }
-        if (newValueX2 > Math.round(globals.image.width() * globals.imageScaleWidth)) {
-            newValueX2 = Math.ceil(globals.image.width() * globals.imageScaleWidth);
-            newValueX1 = $('#x1Field').val();
-        }
-        // update values
-        $('#x1Field').val(newValueX1);
-        $('#x2Field').val(newValueX2);
-        this.reloadSelection();
-    }
-
-    moveSelectionLeft() {
-        // calculate value +/- stepsize times stepsize to account for differing image sizes
-        let newValueX1 = Math.round(parseInt($('#x1Field').val()) - Math.max(1, (globals.moveSelectionStepSize * globals.imageScaleWidth)));
-        let newValueX2 = Math.round(parseInt($('#x2Field').val()) - Math.max(1, (globals.moveSelectionStepSize * globals.imageScaleWidth)));
-        // checking if the box would be out of bounds and puts it to max/min size and doesn't move the other dimension
-        if (newValueX1 < 0) {
-            newValueX1 = 0;
-            newValueX2 = $('#x2Field').val();
-        }
-        if (newValueX2 > Math.round(globals.image.width() * globals.imageScaleWidth)) {
-            newValueX2 = Math.ceil(globals.image.width() * globals.imageScaleWidth);
-            newValueX1 = $('#x1Field').val();
-        }
-        // update values
-        $('#x1Field').val(newValueX1);
-        $('#x2Field').val(newValueX2);
-        this.reloadSelection();
-    }
-
-    increaseSelectionSizeUp() {
-        // calculate value +/- stepsize (times stepsize to account for differing image sizes)
-        let newValueY1 = Math.round(parseInt($('#y1Field').val()) - Math.max(1, (globals.moveSelectionStepSize * globals.imageScaleHeight)));
-        // checking if the box would be out of bounds and puts it to max/min size and doesn't move the other dimension
-        if (newValueY1 < 0) {
-            newValueY1 = 0;
-        }
-
-        // update values
-        $('#y1Field').val(newValueY1);
-        this.reloadSelection();
-    }
-
-    decreaseSelectionSizeFromTop() {
-        // calculate value +/- stepsize times stepsize to account for differing image sizes
-        let newValueY1 = Math.round(parseInt($('#y1Field').val()) + Math.max(1, (globals.moveSelectionStepSize * globals.imageScaleHeight)));
-        let newValueY2 = Math.round(parseInt($('#y2Field').val()));
-
-        if (newValueY2 <= newValueY1) {
-            newValueY1 = newValueY2 - 1;
-        }
-        // update values
-        $('#y1Field').val(newValueY1);
-        this.reloadSelection();
-    }
-
-    increaseSelectionSizeRight() {
-        // calculate value +/- stepsize times stepsize to account for differing image sizes
-        let newValueX2 = Math.round(parseInt($('#x2Field').val()) + Math.max(1, (globals.moveSelectionStepSize * globals.imageScaleWidth)));
-        // checking if the box would be out of bounds and puts it to max/min size and doesn't move the other dimension
-
-        if (newValueX2 > Math.round(globals.image.width() * globals.imageScaleWidth)) {
-            newValueX2 = Math.ceil(globals.image.width() * globals.imageScaleWidth);
-        }
-        // update values
-        $('#x2Field').val(newValueX2);
-        this.reloadSelection();
-    }
-
-    decreaseSelectionSizeFromRight() {
-        // calculate value +/- stepsize times stepsize to account for differing image sizes
-        let newValueX1 = Math.round(parseInt($('#x1Field').val()));
-        let newValueX2 = Math.round(parseInt($('#x2Field').val()) - Math.max(1, (globals.moveSelectionStepSize * globals.imageScaleWidth)));
-
-        if (newValueX1 >= newValueX2) {
-            newValueX2 = newValueX1 + 1;
-        }
-        // update values
-        $('#x2Field').val(newValueX2);
-        this.reloadSelection();
-    }
-
-    cancelSelection() {
-        if (this.selection) {
-            this.selection.cancelSelection();
-        }
-    }
-
-    /**
      * Update the contents of the annotation values
      *
      * @param img
@@ -349,10 +135,10 @@ class BoundingBoxes {
             $('#x' + i + 'Box').remove();
             $('#y' + i + 'Box').remove();
         }
-        $('#x1Field').val(Math.round(selection.x1 * globals.imageScaleWidth));
-        $('#y1Field').val(Math.round(selection.y1 * globals.imageScaleHeight));
-        $('#x2Field').val(Math.round(selection.x2 * globals.imageScaleWidth));
-        $('#y2Field').val(Math.round(selection.y2 * globals.imageScaleHeight));
+        $('#x1Field').val(Math.round(selection.x1));
+        $('#y1Field').val(Math.round(selection.y1));
+        $('#x2Field').val(Math.round(selection.x2));
+        $('#y2Field').val(Math.round(selection.y2));
     }
 
     static getTag(field) {
@@ -368,26 +154,19 @@ class BoundingBoxes {
         this.clear();
     }
 
+    clear() {
+
+        for (let elem of this.hEl.getElements()) {
+            this.viewer.canvas.removeChild(elem.element);
+        }
+        this.hEl.elements = [];
+    }
+
     handleMouseDown(event) {
     }
 
     handleMouseUp(event) {
-        return;
 
-        if (typeof globals.editedAnnotationsId == "undefined")
-            return;
-
-        //let anno = globals.allAnnotations.find(function (e) { return e.id == globals.editedAnnotationsId});
-
-        var left = Math.round($('#x1Field').val() / globals.imageScaleWidth);
-        var right = Math.round($('#x2Field').val() / globals.imageScaleWidth);
-        var top = Math.round($('#y1Field').val() / globals.imageScaleHeight);
-        var bottom = Math.round($('#y2Field').val() / globals.imageScaleHeight);
-
-        // check if we clicked inside that annotation
-        if ((globals.mouseUpX >= left && globals.mouseUpX <= right && globals.mouseUpY >= top && globals.mouseUpY <= bottom) == false) {
-            this.resetSelection(true);
-        }
     }
 
     handleEscape() {
@@ -395,107 +174,8 @@ class BoundingBoxes {
     }
 
     handleMouseClick(event) {
-        // get current annotation type id
-        var annotationType = parseInt($('#annotation_type_id').val());
-
-        // array with all matching annotations
-        var matchingAnnotations = [];
-
-        for (var a in globals.allAnnotations) {
-            var annotation = globals.allAnnotations[a];
-            //if (annotation.annotation_type.id !== annotationType) {
-            //  continue;
-            //}
-            if (annotation.vector === null)
-                continue;
-
-            var left = annotation.vector.x1;
-            var right = annotation.vector.x2;
-            var top = annotation.vector.y1;
-            var bottom = annotation.vector.y2;
-
-            // check if we clicked inside that annotation
-            if (globals.mouseClickX >= left && globals.mouseClickX <= right && globals.mouseClickY >= top && globals.mouseClickY <= bottom) {
-                matchingAnnotations.push(annotation);
-            }
-        }
-
-        // no matches
-        if (matchingAnnotations.length === 0) {
-            return;
-        }
-
-        annotation = matchingAnnotations[0];
-
-        // a single match
-        if (matchingAnnotations.length === 1) {
-            // get the id of the corresponding edit button
-            let edit_button_id = '#annotation_edit_button_' + annotation.id;
-            // trigger click event
-            $(edit_button_id).click();
-        }
-        // multiple matches
-        else {
-            // if we have multiple matching annotations, we have the following descending criteria:
-            // 1. prefer annotation lying inside another one completely
-            // 2. prefer annotation, which left border is to the left of another ones
-            // 3. prefer annotation, which top border is above another ones
-            for (var a1 in matchingAnnotations) {
-                var annotation1 = matchingAnnotations[a1];
-
-                if (annotation.id === annotation1.id)
-                    continue;
-
-                if (this.inside(annotation1, annotation)) {
-                    annotation = annotation1;
-                    continue;
-                }
-
-                if (this.inside(annotation, annotation1)) {
-                    continue;
-                }
-
-                if (this.leftOf(annotation1, annotation)) {
-                    annotation = annotation1;
-                    continue;
-                }
-
-                if (this.leftOf(annotation, annotation1)) {
-                    continue;
-                }
-
-                if (this.above(annotation1, annotation)) {
-                    annotation = annotation1;
-                    continue;
-                }
-
-                if (this.above(annotation, annotation1)) {
-                    continue;
-                }
-            }
-            // get the id of the corresponding edit button
-            let edit_button_id = '#annotation_edit_button_' + annotation.id;
-            // trigger click event
-            $(edit_button_id).click();
-        }
     }
 
     handleMousemove() {
-    }
-
-    // checks if annotation a1 is inside annotation a2
-    inside(a1, a2) {
-        return a1.vector.x1 >= a2.vector.x1 && a1.vector.y1 >= a2.vector.y1 &&
-            a1.vector.x2 <= a2.vector.x2 && a1.vector.y2 <= a2.vector.y2;
-    }
-
-    // checks if the left border of annotation a1 is to the left of the left border of annotation a2
-    leftOf(a1, a2) {
-        return a1.vector.x1 < a2.vector.x1;
-    }
-
-    // checks if the top border of annotation a1 is above the top border of annotation a2
-    above(a1, a2) {
-        return a1.vector.y1 < a2.vector.y1;
     }
 }
