5f01b02a6fffd29232fc298e5e715b55c3d551f1
c.marzahl@de.eu.local <c.marzahl@euroimmun.de>
2019-12-02 16:27:53 +0100

    annotation type change lead to view change

--- imagetagger/imagetagger/annotations/static/annotations/js/boundingboxes.js
+++ imagetagger/imagetagger/annotations/static/annotations/js/boundingboxes.js
@@ -25,7 +25,7 @@ class BoundingBoxes {
         return this.imageid;
     }
 
-    getHitAnnotationVector(){
+    getHitAnnotationVector() {
 
         if (this.selection === undefined)
             return undefined;
@@ -44,9 +44,9 @@ class BoundingBoxes {
                 break;
             default:
                 var length = item.segments.length;
-                for (var i = 0; i < length; i++){
-                    vector["x" + i] = Math.round(item.segments[i].point.x);
-                    vector["y" + i] = Math.round(item.segments[i].point.x);
+                for (var i = 1; i <= length; i++) {
+                    vector["x" + i] = Math.round(item.segments[i - 1].point.x);
+                    vector["y" + i] = Math.round(item.segments[i - 1].point.y);
                 }
                 break;
         }
@@ -63,16 +63,18 @@ class BoundingBoxes {
 
         var canvasObject = undefined;
 
-        switch (selected_annotation_type.vector_type){
+        switch (selected_annotation_type.vector_type) {
             case 2:  // POINT or Elipse
-                var rectangle = new paper.Path.Rectangle(imagePoint, new paper.Size(10,10));
+                var rectangle = new paper.Rectangle(imagePoint,
+                    new paper.Size(selected_annotation_type.default_width, selected_annotation_type.default_hight));
                 canvasObject = new paper.Shape.Ellipse(rectangle);
+                canvasObject.position = imagePoint;
                 canvasObject.data.type = "circle";
 
                 break;
 
             case 3:  // Line
-                canvasObject = new paper.Path.Line(imagePoint, new paper.Point(imagePoint.x + 10, imagePoint.y +10));
+                canvasObject = new paper.Path(imagePoint);
                 canvasObject.data.type = "line";
 
                 break;
@@ -87,7 +89,9 @@ class BoundingBoxes {
                 break;
 
             case 1:  // Rect
-                canvasObject = new paper.Path.Rectangle(imagePoint, new paper.Size(10,10));
+                canvasObject = new paper.Path.Rectangle(imagePoint,
+                    new paper.Size(selected_annotation_type.default_width, selected_annotation_type.default_hight));
+                canvasObject.position = imagePoint;
                 canvasObject.data.type = "rect";
 
                 break;
@@ -95,6 +99,7 @@ class BoundingBoxes {
             default:
                 canvasObject = new paper.Path.Rectangle(imagePoint,
                     new paper.Size(selected_annotation_type.default_width, selected_annotation_type.default_hight));
+                canvasObject.position = imagePoint;
                 canvasObject.data.type = "rect";
 
                 break;
@@ -103,9 +108,9 @@ class BoundingBoxes {
 
         canvasObject.selected = true;
         canvasObject.strokeColor = selected_annotation_type.color_code;
-        canvasObject.strokeWidth = 5; //TODO: Find better solution
+        canvasObject.strokeWidth = 1; //TODO: Find better solution
         canvasObject.name = '~' + new Date().getMilliseconds();
-        canvasObject.fillColor =  new paper.Color(0, 0, 0, 0.000001);
+        canvasObject.fillColor = new paper.Color(0, 0, 0, 0.000001);
 
 
         // bounding box coordinates
@@ -125,8 +130,8 @@ class BoundingBoxes {
         // set object as selected
         this.selection = {
             type: "stroke",
-            item: canvasObject,
-        }
+            item: canvasObject
+        };
 
         // return temp annotation
         return {
@@ -141,7 +146,7 @@ class BoundingBoxes {
             return;
         }
 
-        switch (annotation.annotation_type.vector_type){
+        switch (annotation.annotation_type.vector_type) {
             case 1:  // Rect
             case 6:
                 var rect = new paper.Path.Rectangle(annotation.vector.x1, annotation.vector.y1,
@@ -149,22 +154,22 @@ class BoundingBoxes {
 
                 rect.strokeColor = annotation.annotation_type.color_code;
                 rect.strokeWidth = Math.ceil((annotation.vector.x2 - annotation.vector.x1) / 20);
-                rect.name = '#'+annotation.id;
-                rect.fillColor =  new paper.Color(0, 0, 0, 0.000001);
+                rect.name = '#' + annotation.id;
+                rect.fillColor = new paper.Color(0, 0, 0, 0.000001);
                 rect.data.type = "rect";
 
                 this.group.addChild(rect);
                 break;
 
             case 2:  // POINT or Elipse
-                var rectangle = new paper.Path.Rectangle(annotation.vector.x1, annotation.vector.y1,
+                var rectangle = new paper.Rectangle(annotation.vector.x1, annotation.vector.y1,
                     annotation.vector.x2 - annotation.vector.x1, annotation.vector.y2 - annotation.vector.y1);
-                var ellipse = new paper.Path.Shape.Ellipse(rectangle);
+                var ellipse = new paper.Shape.Ellipse(rectangle);
 
                 ellipse.strokeColor = annotation.annotation_type.color_code;
                 ellipse.strokeWidth = Math.ceil((annotation.vector.x2 - annotation.vector.x1) / 20);
-                ellipse.name = '#'+annotation.id;
-                ellipse.fillColor =  new paper.Color(0, 0, 0, 0.000001);
+                ellipse.name = '#' + annotation.id;
+                ellipse.fillColor = new paper.Color(0, 0, 0, 0.000001);
                 ellipse.data.type = "circle";
 
                 this.group.addChild(ellipse);
@@ -175,8 +180,8 @@ class BoundingBoxes {
                     new paper.Point(annotation.vector.x2, annotation.vector.y2));
 
                 line.strokeColor = annotation.annotation_type.color_code;
-                line.strokeWidth = Math.ceil((annotation.vector.x2 - annotation.vector.x1) / 20);
-                line.name = '#'+annotation.id;
+                line.strokeWidth = 3; // TODO: Find solution
+                line.name = '#' + annotation.id;
                 line.data.type = "line";
 
                 this.group.addChild(line);
@@ -184,20 +189,19 @@ class BoundingBoxes {
 
             case 4:  // MULTI_LINE / POLYGON
             case 5:
-                var poly = paper.Path({
+                var poly = new paper.Path({
                     strokeColor: annotation.annotation_type.color_code,
-                    strokeWidth: Math.ceil((annotation.vector.x2 - annotation.vector.x1) / 20),
-                    name: '#'+annotation.id,
+                    strokeWidth: 5,
+                    name: '#' + annotation.id,
                     closed: true,
-                    fillColor: new Color(0, 0, 0, 0.000001)
+                    fillColor: new paper.Color(0, 0, 0, 0.000001)
                 });
                 poly.data.type = "poly";
 
-                var count = annotation.vector.length;
-                for (var i = 1; count; i++) {
-                    poly.add(new paper.Point(annotation.vector.vector["x" + i], annotation.vector.vector["y" + i]));
+                var count = Object.keys(annotation.vector).length / 2;
+                for (var i = 1; i <= count; i++) {
+                    poly.add(new paper.Point(annotation.vector["x" + i], annotation.vector["y" + i]));
                 }
-                poly.simplify();
 
                 this.group.addChild(poly);
                 break;
@@ -226,10 +230,27 @@ class BoundingBoxes {
     }
 
     removeAnnotation(annotationid) {
-        if (typeof annotationid === 'string') {
-            this.group.children[annotationid].remove();
+        var item = this.getItemFromID(annotationid);
+        if (item !== undefined) {
+            item.remove();
         }
-        this.group.children['#'+annotationid].remove();
+    }
+
+    getItemFromID(annotationid) {
+        var item = undefined;
+        if (typeof annotationid === 'string') {
+            item = this.group.children[annotationid];
+        } else {
+            item = this.group.children['#' + annotationid];
+        }
+        return item;
+    }
+
+    simplifyPoly(annotationid) {
+        var item = this.getItemFromID(annotationid);
+        if (item !== undefined && item.data.type === "poly") {
+            poly.simplify();
+        }
     }
 
     /**
@@ -265,7 +286,7 @@ class BoundingBoxes {
 
     hitTest(point) {
 
-        var hit =  this.group.hitTest(point, this.hitOptions);
+        var hit = this.group.hitTest(point, this.hitOptions);
         if (hit) {
             if (hit.item.name.startsWith('~'))
                 return hit.item.name;
@@ -274,23 +295,74 @@ class BoundingBoxes {
         return undefined;
     }
 
-    updateName(tempName, annotationId){
+    updateName(tempName, annotationId) {
         this.group.getItem({name: tempName}).set({name: '#' + annotationId});
     }
 
-    updateAnnotationType(id, annotation_type){
-        var item = this.group.getItem({name: '#' + id})
-        if (item === undefined){
-            item = this.group.getItem({name: id})
-        }
+    updateAnnotationType(id, annotation_type) {
+        var item = this.getItemFromID(id)
 
-        if (item !== undefined) {
-            item.set({strokeColor: annotation_type.color_code});
-
+        if (item !== null) {
 
             // TODO: Zeiche annotation mit neuem vector typ
+            switch (annotation_type.vector_type) {
+                case 2:  // POINT or Elipse
+                    canvasObject = new paper.Shape.Ellipse(item.bounds);
+                    canvasObject.data.type = "circle";
+
+                    break;
+
+                case 3:  // Line
+                    canvasObject = new paper.Path(item.bounds.topLeft);
+                    canvasObject.add(item.bounds.bottomRight);
+                    canvasObject.data.type = "line";
+
+                    break;
+
+                case 4:  // MULTI_LINE / POLYGON
+                case 5:
+                    var canvasObject = new paper.Path({
+                        closed: true
+                    });
+                    for (var i = 0; i < item.segments.lenght; i++) {
+                        canvasObject.add(item.segments[i].point);
+                    }
+                    canvasObject.data.type = "poly";
+                    break;
+
+                case 1:  // Rect
+                case 6:
+                    canvasObject = new paper.Path.Rectangle(item.bounds);
+                    canvasObject.data.type = "rect";
+
+                    break;
+                default:
+
+                    break;
+
+            }
+            canvasObject.strokeColor = annotation_type.color_code;
         }
+
+        canvasObject.selected = true;
+        canvasObject.strokeWidth = item.strokeWidth;
+        canvasObject.fillColor = new paper.Color(0, 0, 0, 0.000001);
+
+        var tempName = item.name;
+
+        item.remove();
+
+        canvasObject.name = tempName;
+        this.group.addChild(canvasObject);
+
+        // set object as selected
+        this.selection = {
+            type: "stroke",
+            item: canvasObject
+        };
+
     }
+
 
     clear() {
         this.group.removeChildren();
@@ -305,35 +377,68 @@ class BoundingBoxes {
             // Convert from viewport coordinates to image coordinates.
             var imagePoint = new paper.Point(this.viewer.viewport.viewportToImageCoordinates(viewportPoint));
 
+            if (!event.shift) {
 
-            if (this.selection.type == 'fill'){
+                if (this.selection.type == 'fill') {
 
-                var tempRect = this.selection.item.bounds.clone();
-                tempRect.center = imagePoint
+                    var tempRect = this.selection.item.bounds.clone();
+                    tempRect.center = imagePoint;
 
-                if (this.isPointInImage(tempRect.getTopLeft()) && this.isPointInImage(tempRect.getBottomRight()))
-                    this.selection.item.position = imagePoint;
+                    if (this.isPointInImage(tempRect.getTopLeft()) && this.isPointInImage(tempRect.getBottomRight()))
+                        this.selection.item.position = imagePoint;
 
-            } else if (this.selection.item.data.type == "poly"
-                && this.selection.type == 'segment') {
+                } else if (this.selection.item.data.type == "poly") {
 
-                this.selection.segment.point = this.fixPointToImage(imagePoint);
+                    if (this.selection.type == 'segment') {
+                        this.selection.segment.point = this.fixPointToImage(imagePoint);
+                    } else {
+                        this.selection.item.add(imagePoint);
+                    }
 
-            } else {
-                var offset = imagePoint.add(this.selection.item.data.offset_point);
-                var min_x = Math.min(this.selection.item.data.from.x, offset.x);
-                var min_y = Math.min(this.selection.item.data.from.y, offset.y);
-                var max_x = Math.max(this.selection.item.data.from.x, offset.x);
-                var max_y = Math.max(this.selection.item.data.from.y, offset.y);
 
-                if (max_x - min_x < 10) max_x = min_x + 10;
-                if (max_y - min_y < 10) max_y = min_y + 10;
+                } else if (this.selection.item.data.type == "line") {
 
-                // fix annotation to image
-                var topLeft = this.fixPointToImage(new paper.Point(min_x, min_y));
-                var bottomRight =  this.fixPointToImage(new paper.Point(max_x, max_y));
+                    if (this.selection.item.segments.length == 1) {
+                        this.selection.item.add(imagePoint);
 
-                this.selection.item.bounds = new paper.Rectangle(topLeft, bottomRight);
+                    } else {
+                        // check if mouse is near to first, second or center point and move that one
+                        if (this.selection.item.segments[0].point.getDistance(imagePoint)
+                            < this.selection.item.position.getDistance(imagePoint)) {
+                            this.selection.item.segments[0].point = imagePoint;
+
+                        } else if (this.selection.item.segments[1].point.getDistance(imagePoint)
+                            < this.selection.item.position.getDistance(imagePoint)) {
+                            this.selection.item.segments[1].point = imagePoint;
+                        } else {
+                            this.selection.item.position = imagePoint;
+                        }
+                    }
+                } else {
+                    var offset = imagePoint.add(this.selection.item.data.offset_point);
+                    var min_x = Math.min(this.selection.item.data.from.x, offset.x);
+                    var min_y = Math.min(this.selection.item.data.from.y, offset.y);
+                    var max_x = Math.max(this.selection.item.data.from.x, offset.x);
+                    var max_y = Math.max(this.selection.item.data.from.y, offset.y);
+
+                    if (max_x - min_x < 10) max_x = min_x + 10;
+                    if (max_y - min_y < 10) max_y = min_y + 10;
+
+                    // fix annotation to image
+                    var topLeft = this.fixPointToImage(new paper.Point(min_x, min_y));
+                    var bottomRight = this.fixPointToImage(new paper.Point(max_x, max_y));
+
+                    this.selection.item.bounds = new paper.Rectangle(topLeft, bottomRight);
+                }
+            } else { // erase mode
+                if (this.selection.item.data.type == "poly") {
+                    var hit = this.selection.item.hitTest(imagePoint, this.hitOptions);
+                    if (hit !== null && hit.type == 'segment') {
+                        hit.segment.remove();
+                    }
+                    ;
+
+                }
             }
         }
 
@@ -358,12 +463,23 @@ class BoundingBoxes {
                 hitResult.item.bounds.getBottomRight(), hitResult.item.bounds.getTopRight()];
 
             // sort bounding box coordinates by distance to mouse event
-            var sorted = bounding.sort((a,b) => (a.getDistance(point) > b.getDistance(point)) ? 1
+            var sorted = bounding.sort((a, b) => (a.getDistance(point) > b.getDistance(point)) ? 1
                 : ((b.getDistance(point) > a.getDistance(point)) ? -1 : 0));
 
             // save opposite box corner and offset between mouse and next corner
-            hitResult.item.data.from = new paper.Point(sorted[sorted.length-1].x, sorted[sorted.length-1].y);
-            hitResult.item.data.offset_point = new paper.Point(sorted[0].x - point.x, sorted[0].y-point.y);
+            hitResult.item.data.from = new paper.Point(sorted[sorted.length - 1].x, sorted[sorted.length - 1].y);
+            hitResult.item.data.offset_point = new paper.Point(sorted[0].x - point.x, sorted[0].y - point.y);
+
+
+            // if poly add new handling point
+            if (this.selection.type == 'stroke' &&
+                this.selection.item.data.type == 'poly' &&
+                this.selection.item.segments.length > 3) {
+                var location = this.selection.location;
+                if (this.selection.location !== undefined) {
+                    this.selection.item.insert(location.index + 1, point);
+                }
+            }
 
             return parseInt(hitResult.item.name.replace('#', ''));
         }
@@ -386,7 +502,7 @@ class BoundingBoxes {
 
     }
 
-    checkIfAnnotationTypeChangeIsValid(sourceId, targetId){
+    checkIfAnnotationTypeChangeIsValid(sourceId, targetId) {
         const validConversions = {
             1: [1, 2, 3, 4, 5, 6],
             2: [1, 2, 6],
@@ -396,7 +512,7 @@ class BoundingBoxes {
             6: [1, 2, 3, 4, 5, 6],
         }
 
-        if (sourceId in validConversions){
+        if (sourceId in validConversions) {
             return validConversions[sourceId].includes(targetId);
         } else {
             return false;
