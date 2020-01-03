de3246cf9638d77f7241ffae7e0b2799d7d9191f
c.marzahl@de.eu.local <c.marzahl@euroimmun.de>
2020-01-03 13:17:43 +0100

    Added changes for registration version 3

--- imagetagger/imagetagger/settings_base.py
+++ imagetagger/imagetagger/settings_base.py
@@ -64,7 +64,9 @@ ROOT_URLCONF = 'imagetagger.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
