01a2f75529c9a97cb495d410beb21d552f7083cf
Marc Aubreville <marc.aubreville@sivantos.com>
2020-01-31 13:43:12 +0100

    Implemented (untested) create annotation type

--- exact/exact/administration/serializers.py
+++ exact/exact/administration/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework.serializers import ModelSerializer
 
 from exact.administration.models import Product
+from exact.annotations.models import Annotation, AnnotationType
+from typing import Dict, Any
 
 
 class ProductSerializer(ModelSerializer):
@@ -10,3 +12,13 @@ class ProductSerializer(ModelSerializer):
             'id',
             'name',
         )
+
+def serialize_annotationType(annotation_type: AnnotationType) -> Dict[str, Any]:
+    return {
+            'id': annotation_type.id,
+            'closed': annotation_type.closed,
+            'name': annotation_type.name,
+            'vector_type': annotation_type.vector_type,
+            'color_code': annotation_type.color_code,
+            'area_hit_test' : annotation_type.area_hit_test
+    }
