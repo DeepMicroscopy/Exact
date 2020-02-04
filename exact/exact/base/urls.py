dbfd47ebb30d3d35807959580473309bc90f909f
Marc Aubreville <marc.aubreville@gmail.com>
2020-02-04 09:46:41 +0100

    Slide runner sync --> Enhance REST API (#5)
    * Implemented (untested) create annotation type
    
    * Bugfix for inserting proper name of annotation
    
    Bugfix
    
    * Changed uploading of images to API call (enabling upload from SlideRunner)
    
    * Added deletion of annotation type (if unused)
    
    * Removed debug code
    
    * Introduced new API call for image deletion
    
    * Added API call to download single images.
    
    * Removed decorator from create_annotation
    
    * Removed decorator from API request remove annotation
    
    * Bugfix: Save unique_identifier.
    
    * Added option to set last_edit_time upon annotation creation (needed for db sync with SlideRunner)
    
    * Extended annotation update API
    
    * Removed obsolete lookup for (non-unique) unique_identifier (multi-label-case)
    
    * Added API call for time readout to check if syncing is possible.
    
    * Added API call to get overview of imagesets.
    
    * Added products to imageset serializer.

--- exact/exact/base/urls.py
+++ exact/exact/base/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'base'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^timesync/$', views.report_time, name='time sync'),
     url(r'^problems/$', views.problem_report, name='problem')
 ]
