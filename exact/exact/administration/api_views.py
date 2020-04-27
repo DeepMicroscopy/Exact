d3b4ce318fb01fd6f00cf62cdfe79ce2e9e3c298
Christian Marzahl <c.marzahl@euroimmun.de>
2020-04-27 11:32:15 +0200

    Enables filtered navigation

--- exact/exact/administration/api_views.py
+++ exact/exact/administration/api_views.py
@@ -36,18 +36,35 @@ class ProductViewset(viewsets.ModelViewSet):
             return super(ProductViewset, self).list(request, *args, **kwargs)
         else:
             products = self.filter_queryset(self.get_queryset()).order_by('team')
+            
+            current_query = request.META['QUERY_STRING']
+            if "page" not in request.query_params:
+                current_query += "&page=1"
+                page_id = 1
+            else:
+                page_id = int(request.query_params.get('page', 1))            
+            limit = int(request.query_params.get('limit', api_settings.PAGE_SIZE))
 
-            query = request.GET.get('query')
-            get_query = ''
-            paginator = Paginator(products, api_settings.PAGE_SIZE)
-            page = request.GET.get('page')
-            page = paginator.get_page(page)
+            paginator = Paginator(products, limit)
+            page = paginator.get_page(page_id)
+
+
+            previous_query = first_query = current_query.replace("&page="+str(page_id), "&page=1")
+            if page.has_previous():
+                previous_query = current_query.replace("&page="+str(page_id), "&page={}".format(page.previous_page_number()))
+            
+            next_query = last_query = current_query.replace("&page="+str(page_id), "&page={}".format(paginator.num_pages))
+            if page.has_next():
+                next_query = current_query.replace("&page="+str(page_id), "&page={}".format(page.next_page_number()))
+
 
             return TemplateResponse(request, 'base/explore.html', {
                 'mode': 'products',
                 'products': page,  # to separate what kind of stuff is displayed in the view
                 'paginator': page,  # for page stuff
-                'get_query': get_query,
-                'query': query,
+                'first_query': first_query,
+                'previous_query': previous_query,
+                'next_query': next_query,
+                'last_query': last_query,
                 #'filter': self.filterset_class
             })
