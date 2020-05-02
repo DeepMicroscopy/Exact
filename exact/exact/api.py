af116a08b782818b7004a6f52e7ea92c6ca3ab33
Christian Marzahl <christian.marzahl@gmail.com>
2020-05-02 11:54:36 +0200

    Added version control system

--- exact/exact/api.py
+++ exact/exact/api.py
@@ -15,9 +15,11 @@ router_api.register(r'users/team_membership', users_views.TeamMembershipViewset,
 router_api.register(r'images/images', images_views.ImageViewSet, basename='Image')
 router_api.register(r'images/image_sets', images_views.ImageSetViewSet, basename='ImageSet')
 router_api.register(r'images/set_tags', images_views.SetTagViewSet, basename='SetTag')
+router_api.register(r'images/set_versions', images_views.SetVersionViewSet, basename='SetVersion')
 router_api.register(r'images/screening_modes', images_views.ScreeningModeViewSet, basename='ScreeningMode')
 
 router_api.register(r'annotations/annotations', annotations_views.AnnotationViewSet, basename='Annotation')
+router_api.register(r'annotations/annotation_versions', annotations_views.AnnotationVersionViewSet, basename='AnnotationVersion')
 router_api.register(r'annotations/annotation_types', annotations_views.AnnotationTypeViewSet, basename='AnnotationType')
 router_api.register(r'annotations/annotation_media_files', annotations_views.AnnotationMediaFileViewSet, basename='AnnotationMediaFile')
 router_api.register(r'annotations/verifications', annotations_views.VerificationViewSet, basename='Verification')
@@ -30,8 +32,11 @@ router_api.register(r'administration/products', administrations_views.ProductVie
 
 router.register(r'images/image_sets_explore', images_views.ImageSetViewSet, basename='ImageSetView')
 router.register(r'images/images_explore', images_views.ImageViewSet, basename='ImageView')
+router.register(r'images/versions_explore', images_views.SetVersionViewSet, basename='VersionView')
 
 router.register(r'annotations/annotation_types_explore', annotations_views.AnnotationTypeViewSet, basename='AnnotationTypeView')
 router.register(r'annotations/annotations_explore', annotations_views.AnnotationViewSet, basename='AnnotationView')
+router.register(r'annotations/annotation_versions_explore', annotations_views.AnnotationVersionViewSet, basename='AnnotationVersionView')
+
 
 router.register(r'administration/products_explore', administrations_views.ProductViewset, basename='ProductView')
