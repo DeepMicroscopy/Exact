df9e38edc14ed834904c11d8238ed7d7d0003f9d
Haroun Djudzman <haroun.djudzman@gmail.com>
2020-08-24 17:17:26 +0200

    Implemented classification and object detection

--- exact/exact/annotations/static/annotations/js/inference_tool.js
+++ exact/exact/annotations/static/annotations/js/inference_tool.js
@@ -15,23 +15,63 @@ class InferenceTool {
 
     initUiEvents(context) {
 
-        $("#inference_update_btn").click(this.performeInference.bind(this));
+        $("#inference_update_btn").click(this.performInference.bind(this));
 
     }
 
-    performeInference(event) {
-
+    performInference(event) {
+        
+        
+        // Load image from current viewer
         const img = this.viewer.canvas.children[0];
+        const ctx = img.getContext("2d");
 
-        // Load the model.
-        mobilenet.load().then(model => {
-          // Classify the image.
-          model.classify(img).then(predictions => {
-            console.log('Predictions: ');
-            console.log(predictions);
-          });
-        });
-
+        // Check which inference task
+        let sel = document.getElementById('inference_select');
+        
+        if (sel.value == 'classification') {
+            // Load the model.
+            document.getElementById("inf_card").innerHTML = "Loading model"; 
+            mobilenet.load().then(model => {
+                // Classify the image.
+                model.classify(img).then(predictions => {
+                    document.getElementById("inf_card").innerHTML = "Classification completed"; 
+                    //document.getElementById("inf_result_card").innerHTML = predictions[0].className;
+                    console.log('Predictions: ');
+                    console.log(predictions);
+                });
+            });
+        } else if (sel.value == 'object-detection') {
+            // Load the model.
+            document.getElementById("inf_card").innerHTML = "Loading model"; 
+            cocoSsd.load().then(model => {
+                // detect objects in the image.
+                model.detect(img).then(predictions => {
+                    document.getElementById("inf_card").innerHTML = "Object detection completed"; 
+                    console.log('Predictions: ', predictions);
+                    const font = "16px sans-serif";
+                    ctx.font = font;
+                    ctx.textBaseline = "top";
+                    predictions.forEach(prediction => {
+                        const x = prediction.bbox[0];
+                        const y = prediction.bbox[1];
+                        const width = prediction.bbox[2];
+                        const height = prediction.bbox[3];
+                        // Draw the bounding box.
+                        ctx.strokeStyle = "#00FFFF";
+                        ctx.lineWidth = 4;
+                        ctx.strokeRect(x, y, width, height);
+                        // Draw the label background.
+                        ctx.fillStyle = "#00FFFF";
+                        const textWidth = ctx.measureText(prediction.class).width;
+                        const textHeight = parseInt(font, 10); // base 10
+                        ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
+                        ctx.fillStyle = "#000000";
+                        ctx.fillText(prediction.class, x, y);
+                    });
+                });
+            });
+        };
     }
 
     destroy() {
