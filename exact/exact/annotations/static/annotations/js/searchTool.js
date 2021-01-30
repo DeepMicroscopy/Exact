215a20149c1b3b7f331a32b7c5520a73b28b93b3
Christian Marzahl <christian.marzahl@gmail.com>
2021-01-30 11:33:42 +0100

    Enumerate search and filter ID

--- exact/exact/annotations/static/annotations/js/searchTool.js
+++ exact/exact/annotations/static/annotations/js/searchTool.js
@@ -5,6 +5,9 @@ class SearchTool {
         this.image_id = image_id;
         this.viewer = viewer;
         this.exact_sync = exact_sync;
+        this.currentAnnotation = null;
+        this.all_annotations = [];
+        this.search_offset = 2
 
         this.searchFields = ['@id', '@label', '@first editor', '@last editor', '@remark'];
 
@@ -14,13 +17,75 @@ class SearchTool {
     initUiEvents() {
 
         $('#search_update_btn').click(this.updateSearch.bind(this))
+
+        $('#back_button_search').click(this.backOneSearchElement.bind(this)); 
+        $('#next_button_search').click(this.nextOneSearchElement.bind(this)); 
+
+        $('#currentSearchElement').keypress(this.setCurrentAnnotationByKey.bind(this));
+    }
+
+    setCurrentAnnotationByKey(event) {
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if(keycode == '13'){
+            
+            let serach_id = $('#currentSearchElement').val().trim();
+
+            let currentAnnotation = this.all_annotations.find(a => a.id === parseInt(serach_id));
+            if (currentAnnotation !== undefined) {
+                this.currentAnnotation = currentAnnotation;
+
+                let annotation = this.currentAnnotation;
+                this.viewer.raiseEvent('search_ShowAnnotation', { annotation });
+            } else {
+                $("#currentSearchElement").notify("Id not found", { position: "bottom center", className: "error" });
+            }
+
+        }
+    }
+
+    backOneSearchElement(event) {
+
+        let currentIndex = this.all_annotations.indexOf(this.currentAnnotation)
+
+        if (currentIndex -1 > 0) {
+            currentIndex -= 1;
+            this.updateTable(this.all_annotations, Math.max(currentIndex-this.search_offset, 0), currentIndex + this.search_offset)
+
+            this.currentAnnotation = this.all_annotations[currentIndex];
+
+            $("#currentSearchElement").val(this.currentAnnotation.id);
+
+            let annotation = this.currentAnnotation;
+            this.viewer.raiseEvent('search_ShowAnnotation', { annotation });
+        }
+    }
+
+    nextOneSearchElement(event) {
+
+        let currentIndex = this.all_annotations.indexOf(this.currentAnnotation)
+
+        if (currentIndex +1 < this.all_annotations.length) {
+
+            currentIndex += 1;
+            this.updateTable(this.all_annotations, Math.max(currentIndex-2, 0), currentIndex+2)
+
+            this.currentAnnotation = this.all_annotations[currentIndex];
+
+            $("#currentSearchElement").val(this.currentAnnotation.id);
+
+            let annotation = this.currentAnnotation;
+            this.viewer.raiseEvent('search_ShowAnnotation', { annotation });
+        }
     }
 
     raiseSearchedAnnotationClicked(event) {
 
         let unique_identifier = event.currentTarget.dataset.unique_identifier;
-        let annotation = this.exact_sync.annotations[unique_identifier];
+        this.currentAnnotation = this.exact_sync.annotations[unique_identifier];
 
+        $("#currentSearchElement").val(this.currentAnnotation.id);
+
+        let annotation = this.currentAnnotation;
         this.viewer.raiseEvent('search_ShowAnnotation', { annotation });
     }
 
@@ -29,25 +94,19 @@ class SearchTool {
         if (searchText.hasOwnProperty('originalEvent'))
             searchText.preventDefault();
 
-        // save found annotations
-        var all_annotations = []
-
         if (searchText === undefined || searchText.hasOwnProperty('originalEvent')) {
             searchText = $('#searchInputAnnotation').val().trim().toLowerCase();
         }
 
-        var table = document.getElementById('annotationSearchResults');
+        $("#currentSearchElement").val("");
 
-        // remove all elements from table expect first
-        while (table.childNodes.length > 3) {
-            table.removeChild(table.lastChild);
-        }
+        // save found annotations
+        var all_annotations = Object.values(this.exact_sync.annotations)
 
         if (searchText) {
 
             if (this.searchFields.some(searchField => searchText.includes(searchField))) {
 
-                all_annotations = Object.values(this.exact_sync.annotations)
                 let search_requests = searchText.split(';');
 
                 search_requests.forEach(function (item, index) {
@@ -100,63 +159,78 @@ class SearchTool {
                                 break;
                         }
                     }
-                });
-
-                for (const item of all_annotations.slice(0, 10)) {
-                    var row = document.createElement("tr");
-
-                    // ID
-                    var column = document.createElement("th");
-                    var id_link = document.createElement("a");
-                    id_link.textContent = item.id;
-                    id_link.setAttribute("data-unique_identifier", item.unique_identifier);
-                    id_link.onclick = this.raiseSearchedAnnotationClicked.bind(this);
-                    column.appendChild(id_link);
-                    row.appendChild(column);
-
-                    // Label
-                    column = document.createElement("th");
-                    var label = document.createElement("label");
-                    label.innerText = item.annotation_type.name;
-                    column.appendChild(label);
-                    row.appendChild(column);
-
-                    // First Editor
-                    var column = document.createElement("th");
-                    var name_link = document.createElement("a");
-                    name_link.setAttribute('href', include_server_subdir(`/users/user/${item.user.id}/`));
-                    name_link.textContent = item.user.username;
-                    column.appendChild(name_link);
-                    row.appendChild(column);
-
-                    // First Editor
-                    var column = document.createElement("th");
-                    name_link = document.createElement("a");
-                    name_link.setAttribute('href', include_server_subdir(`/users/user/${item.last_editor.id}/`));
-                    name_link.textContent = item.last_editor.username;
-                    column.appendChild(name_link);
-                    row.appendChild(column);
-
-                    // verified
-                    column = document.createElement("th");
-                    var verified = document.createElement("label");
-                    verified.innerText = item.verified_by_user.toString();
-                    column.appendChild(verified);
-                    row.appendChild(column);
-
-                    // remark
-                    column = document.createElement("th");
-                    var remark = document.createElement("label");
-                    remark.innerText = item.remark;
-                    column.appendChild(remark);
-                    row.appendChild(column);
-
-                    table.appendChild(row);
-                } 
+                }); 
             }
         }
 
+        this.all_annotations = all_annotations
+
+        this.updateTable(this.all_annotations)
+
         return all_annotations;
+    }
+
+    updateTable(all_annotations, start=0, end=10) {
+
+        var table = document.getElementById('annotationSearchResults');
+
+        // remove all elements from table expect first
+        while (table.childNodes.length > 3) {
+            table.removeChild(table.lastChild);
+        }
+
+
+        for (const item of all_annotations.slice(start, end)) {
+            var row = document.createElement("tr");
+
+            // ID
+            var column = document.createElement("th");
+            var id_link = document.createElement("a");
+            id_link.textContent = item.id;
+            id_link.setAttribute("data-unique_identifier", item.unique_identifier);
+            id_link.onclick = this.raiseSearchedAnnotationClicked.bind(this);
+            column.appendChild(id_link);
+            row.appendChild(column);
+
+            // Label
+            column = document.createElement("th");
+            var label = document.createElement("label");
+            label.innerText = item.annotation_type.name;
+            column.appendChild(label);
+            row.appendChild(column);
+
+            // First Editor
+            var column = document.createElement("th");
+            var name_link = document.createElement("a");
+            name_link.setAttribute('href', include_server_subdir(`/users/user/${item.user.id}/`));
+            name_link.textContent = item.user.username;
+            column.appendChild(name_link);
+            row.appendChild(column);
+
+            // First Editor
+            var column = document.createElement("th");
+            name_link = document.createElement("a");
+            name_link.setAttribute('href', include_server_subdir(`/users/user/${item.last_editor.id}/`));
+            name_link.textContent = item.last_editor.username;
+            column.appendChild(name_link);
+            row.appendChild(column);
+
+            // verified
+            column = document.createElement("th");
+            var verified = document.createElement("label");
+            verified.innerText = item.verified_by_user.toString();
+            column.appendChild(verified);
+            row.appendChild(column);
+
+            // remark
+            column = document.createElement("th");
+            var remark = document.createElement("label");
+            remark.innerText = item.remark;
+            column.appendChild(remark);
+            row.appendChild(column);
+
+            table.appendChild(row);
+        }
     }
 
     destroy() {
