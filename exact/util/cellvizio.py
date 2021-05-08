a24413bb924f731e02f713494509de10df3265e8
Christian Marzahl <christian.marzahl@gmail.com>
2021-05-08 14:18:50 +0200

    Added funtion for CellVisio

--- exact/util/cellvizio.py
+++ exact/util/cellvizio.py
@@ -62,11 +62,13 @@ class ReadableCellVizioMKTDataset():
        self.fi.nImages=1000
 
        self.fi.width = 576
-       if ((self.fi.size/(2*self.fi.width))%2!=0):
+       if ((self.fi.size / (2 * self.fi.width)) % 2 != 0):
             self.fi.width=512
             self.fi.height=int(self.fi.size/(2*self.fi.width))
-       else:
-            self.fi.height=int(self.fi.size/(2*self.fi.width))
+
+       if abs(self.fi.width - self.fi.height) > 100:
+           self.fi.width = 448
+           self.fi.height = int(self.fi.size / (2 * self.fi.width))
 
        self.filestats = stat(self.fileName)
        self.fi.nImages = int((self.filestats.st_size-self.fi.offset) / (self.fi.size+self.fi.gapBetweenImages))
