f4533c9f2d8c8ba1c639e5264adcc6648ff4592a
Christian Marzahl <christian.marzahl@gmail.com>
2021-05-08 15:15:16 +0200

    Removed fake class

--- exact/exact/annotations/static/annotations/js/exact-browser-sync.js
+++ exact/exact/annotations/static/annotations/js/exact-browser-sync.js
@@ -143,7 +143,6 @@ class EXACTBrowserSync {
     _createNewChannel(channelName) {
 
         try {
-            return new FakeBroadcastChannel(channelName);
             return new BroadcastChannel(channelName);
         } catch (error) {
             console.error(error);
