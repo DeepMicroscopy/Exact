67e59d1a2c2aecf003b2838a9cb392e01de2624b
OettlM <82029523+OettlM@users.noreply.github.com>
2021-08-06 10:17:51 +0200

    Addes empty functions for scissors and glue

--- exact/exact/annotations/static/annotations/js/boundingboxes.js
+++ exact/exact/annotations/static/annotations/js/boundingboxes.js
@@ -227,6 +227,20 @@ class BoundingBoxes {
         return resultDict;
     }
 
+    polyScissorOperation()
+    {
+        var resultDict = {deleted: [], insert: [], update: [], included: []}
+
+        return resultDict
+    }
+
+    polyGlueOperation()
+    {
+        var resultDict = {deleted: [], insert: [], update: [], included: []}
+
+        return resultDict 
+    }
+
     uuidv4() {
         return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
           var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
