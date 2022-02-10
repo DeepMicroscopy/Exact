4ce17419ac04f42c9125f0370dc315c98e93a5ae
FraukeWilm <38855399+FraukeWilm@users.noreply.github.com>
2022-02-10 13:18:07 +0100

    added catch dataset to demo datasets (#78)

--- exact/exact/datasets/urls.py
+++ exact/exact/datasets/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
 
     # SDATA
     path('mitos_wsi_cmc/create/', views.create_mitos_wsi_cmc_dataset, name='create-mitos-wsi-cmc-dataset'),
+    path('catch/create/', views.create_catch_dataset, name='create_catch_dataset'),
 
     #path('eiph/create/', views.create_eiph_dataset, name='create-eiph-dataset'),
     #path('asthma/create/', views.create_asthma_dataset, name='create-asthma-dataset'),
