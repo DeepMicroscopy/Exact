4f203c7fa79ffc82166b882761a92e5dc5f76d4b
Marc Aubreville <marc.aubreville@thi.de>
2024-03-10 15:42:13 +0100

    Migration to more recent django.

--- exact/exact/annotations/migrations/0023_auto_20180428_1756.py
+++ exact/exact/annotations/migrations/0023_auto_20180428_1756.py
@@ -4,8 +4,7 @@ from __future__ import unicode_literals
 
 import datetime
 from django.db import migrations, models
-from django.utils.timezone import utc
-
+from datetime import UTC as utc
 
 class Migration(migrations.Migration):
 
