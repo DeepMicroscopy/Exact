495eab7c498a4ef55444981234674c2345e32d4d
wilm <frauke.wilm@fau.de>
2024-05-31 14:14:50 +0200

    Make hdf5 upload more generic by automatically searaching for dictionary keys

--- exact/exact/images/models.py
+++ exact/exact/images/models.py
@@ -384,18 +384,17 @@ class Image(models.Model):
 
                 elif path.suffix.lower().endswith(".hdf5") :                          
                     with h5py.File(str(path), 'r') as hf:
-                        data = hf["segmentation"]
+                        hdf_path = Path(path)
+                        self.filename = hdf_path.name
+                        key = list(hf.keys())[0] # Only create overlay for first element in hdf5 file
+                        data = hf[key]
                         ndarray_data = np.array(data)
-
                         scaled_image_data = (ndarray_data * (255 / len(np.unique(ndarray_data)))).astype(np.uint8)
                         colored_image = cv2.applyColorMap(scaled_image_data, cv2.COLORMAP_VIRIDIS)
                         colored_image = cv2.cvtColor(colored_image, cv2.COLOR_BGR2RGB)
-
                         vi = pyvips.Image.new_from_array(colored_image)
-                        path = Path(path).with_suffix('.tiff')
+                        path = hdf_path.with_stem(hdf_path.stem + "_{}".format(key)).with_suffix('.tiff')
                         vi.tiffsave(str(path), tile=True, compression='lzw', bigtiff=True, pyramid=True, tile_width=256, tile_height=256)
-                        self.filename = path.name
-
                 else:                            
                     path = Path(path).with_suffix('.tiff')
 
