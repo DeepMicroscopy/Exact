efa6e56b0fdc1ac8a70dbcea1237abcb8f2b5346
Marc Aubreville <marc.aubreville@thi.de>
2024-06-11 15:50:11 +0200

    Bugfix (syntax error).

--- exact/util/tiffzstack.py
+++ exact/util/tiffzstack.py
@@ -229,9 +229,7 @@ class PyramidalZStack:
     @property
     def default_frame(self) -> int:
         """ returns the default frame """
-        if 'PositionZ' not in self.metadata[m]:
-            return 0
-        zpos_vector = [self.metadata[m]['PositionZ'] for m in self.metadata]
+        zpos_vector = [self.metadata[m]['PositionZ'] for m in self.metadata if 'PositionZ' not in self.metadata[m]]
         if 0 in zpos_vector:
             return zpos_vector.index(0)
         # default case: return 0
