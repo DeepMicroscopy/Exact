b63fbc46d24ac72b4170438f9f835875ff6630a6
wilm <frauke.wilm@fau.de>
2024-08-27 14:51:32 +0200

    Show last logged-in info in Django admin panel

--- exact/exact/users/admin.py
+++ exact/exact/users/admin.py
@@ -3,7 +3,10 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import Team, TeamMembership, User, UI_User
 
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'last_login')
+
 admin.site.register(Team)
 admin.site.register(TeamMembership)
-admin.site.register(User, UserAdmin)
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(UI_User)
